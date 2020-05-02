9855f58513e184804a85ae2c0ab216388161ca97
kev87ian <kelvinian87@gmail.com>
2020-05-02 15:48:32 +0300

    Generate getter for endangered class

--- src/main/java/Endangered.java
+++ src/main/java/Endangered.java
@@ -1,2 +1,33 @@
-public class Endangered {
+public class Endangered extends Animal {
+    private String health;
+    private final String state = "Endangered";
+
+    public Endangered (String name, String ranger, String location, String health, String age){
+        this.name = name;
+        this.ranger = ranger;
+        this.location = location;
+        this.health = health;
+        this.age = age;
+    }
+public String getState(){
+        return state;
+}
+    public String getName(){
+        return name;
+    }
+    public String getAge(){
+        return age;
+    }
+    public String getHealth(){
+        return health;
+    }
+    public String getRangerName(){
+        return ranger;
+    }
+    public String getLocation(){
+        return location;
+    }
+    public int getId(){
+        return id;}
+
 }
