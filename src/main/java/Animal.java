7891242837dd8a6b8842cbfc9dc4ba5a5246f9b4
kev87ian <kelvinian87@gmail.com>
2020-05-02 15:40:01 +0300

    Generate animal class

---
+++ src/main/java/Animal.java
@@ -0,0 +1,9 @@
+public abstract class Animal {
+        public String ranger;
+        public String location;
+        public String name;
+        public String age;
+        public int id;
+    }
+
+
